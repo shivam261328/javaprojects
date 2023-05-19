2ab1e54d76cd3de131f40c9f9ae8be41f8442538
shivam261328 <106868499+shivam261328@users.noreply.github.com>
2023-05-20 00:21:43 +0530

    Add files via upload

---
+++ printpattern.java
@@ -0,0 +1,12 @@
+public class printpattern {
+
+    public static void main(String arg[]) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j<= 5-i+1; j++) {
+                System.out.print("*  ");
+
+            }
+            System.out.println("");
+        }
+    }
+}

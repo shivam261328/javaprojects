5789089c45c0a3fad23590fae1b530e0e7ab366d
shivam261328 <106868499+shivam261328@users.noreply.github.com>
2023-05-20 00:28:41 +0530

    Add files via upload

---
+++ nestedforloop.java
@@ -0,0 +1,15 @@
+
+
+public class nestedforloop {
+    public static void main(String arg[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print(i+"   "+j+"   ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
